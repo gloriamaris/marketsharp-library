eaa84429856aabe6ed5d4710140ca72b9690ad24
gloriamaris <monique.dingding@gmail.com>
2018-11-28 21:56:02 +0800

    renamed marketsharp file directory

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 // Change the file folder name here if you're not using default
-define('MARKETSHARP_DIRECTORY', $_SERVER['DOCUMENT_ROOT'] . '/marketsharp-sdk/');
+define('MARKETSHARP_DIRECTORY', $_SERVER['DOCUMENT_ROOT'] . '/marketsharp-library/');
 ?>
